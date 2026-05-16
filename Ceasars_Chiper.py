f4ebff2b9652f4266d534e305e1c0170f2847af3
mizatt <mate175@gmail.com>
2026-05-16 15:10:18 -0300

    Commiting day 8 exercise.

---
+++ Ceasars_Chiper.py
@@ -0,0 +1,94 @@
+#Imports:
+import Cyphers_Ascii as ca
+
+alphabet = list("abcdefghijklmnopqrstuvwxyz") * 2 #*2 foi sugestão do Google após eu pesquisar por estouro de índice.
+
+def process_message():
+    encrypted_letter = []
+    encode_or_decode = get_encode_or_decode()
+    message = get_message()
+    shift_number = get_shift_number()
+
+    if encode_or_decode == 'encode': 
+        for i in message:
+            if not i.isalpha():
+                encrypted_letter.append(i)
+                continue
+            else:
+                position = alphabet.index(i)
+                position += shift_number
+                encrypted_letter.append(alphabet[position])
+    elif encode_or_decode == 'decode':
+        for i in message:
+            if not i.isalpha():
+                encrypted_letter.append(i)
+                continue
+            else:
+                position = alphabet.index(i)
+                position -= shift_number
+                encrypted_letter.append(alphabet[position])
+            
+    print(f"Here's your message: {''.join(encrypted_letter)}")
+    
+#Gets input for encode or decode:
+def get_encode_or_decode():    
+    try:
+        encode_or_decode = input("Type 'encode' to encrypt, type 'decode' to decrypt: ").lower()  
+        if encode_or_decode not in ['encode', 'decode']:
+            raise ValueError("You must choose either 'encode' or 'decode'.")
+    except ValueError as e:
+        print(f"Error: {e}")    
+        return get_encode_or_decode()
+    
+    return encode_or_decode
+
+#Gets input for message:
+def get_message():
+    try:
+        message = input("Type your message: ").lower()
+        if not message.strip():
+            raise ValueError("You must type a message before encoding or decoding.")
+    except ValueError as e:
+        print(f"Error: {e}")
+        return get_message()    
+    return message
+
+#Gets input for shift number:
+def get_shift_number():
+    try:
+        shift_number = int(input("Type the shift number: "))
+        if shift_number <= 0:
+            raise ValueError("Invalid input. Please enter a number greater than zero.")
+    except ValueError as e:
+        print(f"Error: {e}")
+        return get_shift_number()
+    
+    return shift_number
+
+#Gets input for go again indicator:
+def get_go_again_indicator():
+    go_again_indicator = ""
+    go_again_indicator = input("Type 'yes' if you want to go again. Otherwise type 'no': ")
+
+    try:
+        if go_again_indicator not in ['yes', 'no']:
+            raise ValueError("Invalid input. Please type only yes or no.")
+    except ValueError as e:
+        print(f"Error: {e}")
+        return get_go_again_indicator()
+
+    return go_again_indicator
+
+#Prints the art for the Cypher:
+def print_ciphers_art():
+    print(ca.caesar_cipher_logo)
+
+def main():
+    print_ciphers_art()
+    while True:
+        process_message()
+        if get_go_again_indicator() == "no":
+            break
+
+if __name__ == "__main__":
+    main()
---
+++ Day8/Side_Tasks/love_calculator.py
@@ -0,0 +1,35 @@
+first_person_name = ""
+second_person_name = ""
+
+true = ["t","r","u","e"]
+love = ["l","o","v","e"]
+
+def get_second_person_name(true_counter, love_counter):
+    second_person_name = input("Please type the second person's name: ").lower()
+    for i in range(len(second_person_name)):
+        if second_person_name[i] in true:
+            true_counter += 1            
+        if second_person_name[i] in love:
+            love_counter += 1    
+    return true_counter, love_counter
+
+def get_first_person_name():
+    true_counter = 0
+    love_counter = 0
+    first_person_name = input("Please type the first person's name: ").lower()
+    for i in range(len(first_person_name)):
+        if first_person_name[i] in true:
+            true_counter += 1            
+        if first_person_name[i] in love:
+            love_counter += 1   
+    return true_counter, love_counter        
+    
+
+def main():
+    true_counter, love_counter = get_first_person_name()
+    true_counter, love_counter = get_second_person_name(true_counter,love_counter)
+    print(f"Your Love Score is: {true_counter}{love_counter}")
+
+
+if __name__ == "__main__":
+    main()
