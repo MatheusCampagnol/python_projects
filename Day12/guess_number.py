b9b688310747b457096708b8e635f21eafda2ae6
mizatt <mate175@gmail.com>
2026-05-27 20:03:18 -0300

    ---

--- Day12/guess_number.py
+++ Day12/guess_number.py
@@ -1,5 +1,4 @@
 from rich import print
-<<<<<<< HEAD
 from rich.progress import track
 import ascii_art as aa
 import time
@@ -43,15 +42,3 @@ def main():
 if __name__ == "__main__":
     main()
     
-=======
-import ascii_art as aa
-
-def print_ascii():
-    print(aa.guess_art)
-
-def main(): 
-    print_ascii()
-
-if __name__ == "__main__":
-    main()
->>>>>>> a141afa465a851f6758d382a09614763dad2a82d
